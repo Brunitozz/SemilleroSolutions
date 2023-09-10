47d1494fd8c01cc008dc883822f3f09cbd440151
BRUNOPUMA <122190257+Brunitozz@users.noreply.github.com>
2023-09-09 19:19:11 -0500

    arreglos y booleanos

--- Notas_Semillero/notas1.cpp
+++ Notas_Semillero/notas1.cpp
@@ -3,7 +3,7 @@ int edad;
 edad=15;
 
 //Declaración de tipo
-int arr_edades[n] //Dice la dimesion*/
+int arr_enteros[n] //Dice la dimesion*/
 
 
 //Existe valores BAsurita
@@ -12,5 +12,15 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int arr_edades[10]
+    //Arreglo de enteros
+    int arr_enteros[10];
+    for(int i=0; i<10; i++){
+        cout<<i<<", "<<arr_enteros[i]<<", "<<&arr_enteros<<endl;
+    }
+    //Arreglo de booleanos
+    int arr_bool[10];
+    for(int i=0; i<10; i++){
+        cout<<i<<", "<<arr_bool[i]<<", "<<&arr_bool<<endl;
+    }
+    return 0;
 }
