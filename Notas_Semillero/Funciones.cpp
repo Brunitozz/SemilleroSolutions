bcc3f1e72e8fffbeaabbee72ca7027a9075c5f19
BRUNOPUMA <122190257+Brunitozz@users.noreply.github.com>
2023-09-09 19:35:29 -0500

    funciones

---
+++ Notas_Semillero/Funciones.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool sol(int a, int b);
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cout<<cont;
+    return 0;
+}
+
+int sol(int arr){
+    int cont=0
+    int arr[]= {10, 15, 12, 14, 19, 16, 17, 6};
+    for (int i=0; i<8; i++){
+        if(arr[i]%2==0) cont++;
+    }
+    return cont;
+}
