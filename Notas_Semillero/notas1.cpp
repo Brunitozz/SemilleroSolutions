68f53f531f9aa2e60bbe2c84a32e814ee339e4c6
BRUNOPUMA <122190257+Brunitozz@users.noreply.github.com>
2023-09-09 19:14:16 -0500

    algunas modificaciones y notas

---
+++ Notas_Semillero/notas1.cpp
@@ -0,0 +1,16 @@
+/*//Arreglos
+int edad;
+edad=15;
+
+//Declaración de tipo
+int arr_edades[n] //Dice la dimesion*/
+
+
+//Existe valores BAsurita
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int arr_edades[10]
+}
